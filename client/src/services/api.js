e969847e79be59861bddf284f1bbccdd9a42e76d
happlejoseph <happlejoseph66@gmail.com>
2026-09-17 20:31:55 +0530

    change the base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api'
+    baseURL: 'https://hospital-management-system-xoi7.onrender.com/api'
 });
 
 api.interceptors.request.use((config)=> {
